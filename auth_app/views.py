9659e7001f2bf5c219458e8d737d0ebcaf4a7c23
vatrushalol <vatrushkalol@gmail.com>
2023-02-01 16:33:29 +0300

    Добавлены представления к моделям

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,19 +1,45 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
+from django.contrib.auth import authenticate, login, logout
+from .models import User
+
+
 # Create your views here.
 
-def login(request):
-    return HttpResponse('<div><h1><b>Здесь находится страница входа пользователя на сайт</b></h1></div>')
+def log_in(request):
+    if request.method == 'POST':
+        data = request.POST
+        user = authenticate(email=data['email'], password=data['password'])
+        if user and user.is_active:
+            login(request, user)
+            return redirect('index')
+        else:
+            return HttpResponse('Ваш аккаунт заблокирован')
+    else:
+        return render(request, 'login.html')
+
 
 def register(request):
-    return HttpResponse('<div><h1><b>Здесь находится страница регистрации пользователя</b></h1></div>')
+    if request.method == 'POST':
+        data = request.POST
+        user = User(email=data['email'], first_name=data['first_name'], last_name=data['last_name'],
+                    description=data['description'], birthday=data['birthday'], avatar=data['avatar'])
+        user.set_password(data['password'])
+        user.save()
+        login(request, user)
+        return redirect('index.html')
+    else:
+        return render(request, 'register.html')
 
-def logout(request):
-    return HttpResponse('<div><h1><b>Здесь находится страница выхода из учетной записи и переход на главную страницу</b></h1></div>')
+
+def log_out(request):
+    logout(request)
+    return redirect('login')
+
 
 def change_password(request):
     return HttpResponse('<div><h1><b>Здесь находится страница смены пароля пользователя</b></h1></div>')
 
+
 def reset_password(request):
     return HttpResponse('<div><h1><b>Здесь находится страница восстановления пароля</b></h1></div>')
-
