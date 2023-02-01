4de44047d75246667b7f580f4af5f7a1a3c4dfd8
vatrushalol <vatrushkalol@gmail.com>
2023-02-01 16:32:07 +0300

    Добавлены имена для путей

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('login/', login),
-    path('register/', register),
-    path('logout/', logout),
-    path('change-password/', change_password),
-    path('reset-password/', reset_password)
+    path('login/', log_in, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', log_out, name='logout'),
+    path('change-password/', change_password, name='change_password'),
+    path('reset-password/', reset_password, name='reset_password')
 ]
