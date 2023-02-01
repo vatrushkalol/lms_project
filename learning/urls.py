0fe1cc77a91e0304bef99e0e4febaf91c251bf7c
vatrushalol <vatrushkalol@gmail.com>
2023-02-01 16:32:17 +0300

    Добавлены имена для путей

--- learning/urls.py
+++ learning/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('create/', create, name='create'),
-    re_path('^delete/(?P<course_id>[1-9]*)/$', delete, name='delete'),
-    path('detail/<str:title>/', detail, name='detail'),
-    path('enroll/<str:title>/', enroll, name='enroll')
+    path('delete/<int:course_id>/', delete, name='delete'),
+    path('detail/<int:course_id>/', detail, name='detail'),
+    path('enroll/<int:course_id>/', enroll, name='enroll')
 ]
