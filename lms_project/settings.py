f34a90b27a6fba3934f942ef42004a884ab91d86
vatrushalol <vatrushkalol@gmail.com>
2023-02-01 16:30:29 +0300

    Добавлен путь к templates

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'lms_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,7 +130,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/img',
+    BASE_DIR / 'static/styles',
+    BASE_DIR / 'static/scripts'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
