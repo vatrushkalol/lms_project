c5428f72af979623e430255a268e8a32baae363d
vatrushalol <vatrushkalol@gmail.com>
2023-02-14 17:08:18 +0300

    Добавлена возможность регистрации групп

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -4,5 +4,4 @@ from django.contrib.auth.models import Group
 
 # Register your models here.
 admin.site.register(User)
-admin.site.unregister(Group)
 admin.site.site_header = 'Learning Management System'
