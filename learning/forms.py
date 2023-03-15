739f1b2dcee052ef810c1a99c821fa8e11e8926e
vatrushalol <vatrushkalol@gmail.com>
2023-03-16 00:48:52 +0300

    Добавлена поддержка нескольких авторов

--- learning/forms.py
+++ learning/forms.py
@@ -5,7 +5,7 @@ from django import forms
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
-        fields = ('title', 'author', 'description', 'start_date', 'duration', 'price', 'count_lessons')
+        fields = ('title', 'authors', 'description', 'start_date', 'duration', 'price', 'count_lessons')
 
 class ReviewForm(forms.ModelForm):
 
