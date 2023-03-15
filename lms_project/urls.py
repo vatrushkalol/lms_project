67175c8e74a444bfe317f94f6a5752de4045c8f2
vatrushalol <vatrushkalol@gmail.com>
2023-03-16 00:49:25 +0300

    Добавлен дебаг тулбар

--- lms_project/urls.py
+++ lms_project/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('auth_app.urls')),
-    path('courses/', include('learning.urls'))
+    path('courses/', include('learning.urls')),
+    path('__debug__', include('debug_toolbar.urls'))
 ]
 
 if settings.DEBUG:
