3b20eb4ff07fc6bd1df6a6f3800263f4f1949d30
vatrushalol <vatrushkalol@gmail.com>
2023-03-15 21:13:09 +0300

    Написание отзывов для админ панели

--- learning/admin.py
+++ learning/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Course, Lesson
+from .models import Course, Lesson, Review
 
 # Register your models here.
 @admin.register(Course)
@@ -19,3 +19,9 @@ class LessonAdmin(admin.ModelAdmin):
     actions_on_top = True
     actions_on_bottom = True
     actions_selection_counter = True
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'content')
+    search_fields = ('content', )
+    list_per_page = 100
