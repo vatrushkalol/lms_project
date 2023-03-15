8d72cf4990f5cc7a540cf248e8d7ccaf5a6a4fdb
vatrushalol <vatrushkalol@gmail.com>
2023-03-15 21:12:37 +0300

    Добавлена функция написания отзывов

--- learning/views.py
+++ learning/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from django.http import HttpResponse
+from django.core.exceptions import NON_FIELD_ERRORS
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DeleteView, DetailView, CreateView, UpdateView
 from datetime import datetime
-from .forms import CourseForm
+from .forms import CourseForm,  ReviewForm
 from django.urls import reverse
-from .models import Course, Lesson, Tracking
+from .models import Course, Lesson, Tracking, Review
 
 
 # Create your views here.
@@ -74,7 +75,8 @@ class CourseDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(CourseDetailView, self).get_context_data(**kwargs)
-        context['lessons'] = Lesson.objects.filter(course=self.kwargs.get(CourseDetailView.pk_url_kwarg))
+        context['lessons'] = Lesson.objects.filter(course=self.kwargs.get('course_id'))
+        context['reviews'] = Review.objects.filter(course=self.kwargs.get('course_id'))
         return context
 
 
@@ -93,3 +95,21 @@ def enroll(request, course_id):
             records = [Tracking(lesson=lesson, user=request.user, passed=False) for lesson in lessons]
             Tracking.objects.bulk_create(records)
             return HttpResponse('Вы записаны на данный курс')
+
+
+@login_required
+@permission_required('learning.add_review', raise_exception=True)
+def review(request, course_id):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.errors:
+            errors = form.errors[NON_FIELD_ERRORS]
+            return render(request, 'review.html', {'form': form, 'errors': errors})
+        if form.is_valid():
+            data = form.cleaned_data
+            Review.objects.create(content=data['content'], course=Course.objects.get(id=course_id), user=request.user)
+            return redirect(reverse('detail', kwargs={'course_id': course_id}))
+    else:
+        form = ReviewForm()
+        return render(request, 'review.html', {'form': form})
+
