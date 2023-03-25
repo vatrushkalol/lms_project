7eacb896c6c9fe968f04c739ae22f07125d25fe6
vatrushalol <vatrushkalol@gmail.com>
2023-03-25 10:50:58 +0300

    Переделеаны представления регитсрации и логина

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,38 +1,40 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.conf import settings
+from django.contrib.auth.views import LoginView
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import Group
+from datetime import datetime
 from .models import User
+from django.views.generic.edit import CreateView
+from .forms import LoginForm, RegisterForm
 
 
-# Create your views here.
+class UserLoginView(LoginView):
+    authentication_form = LoginForm
+    template_name = 'login.html'
+    next_page = 'index'
 
-def log_in(request):
-    if request.method == 'POST':
-        data = request.POST
-        user = authenticate(email=data['email'], password=data['password'])
-        if user and user.is_active:
-            login(request, user)
-            return redirect('index')
-        else:
-            return HttpResponse('Ваш аккаунт заблокирован')
-    else:
-        return render(request, 'login.html')
+    def form_valid(self, form):
+        is_remeber = self.request.POST.get('is_remember')
+        if is_remeber == 'on':
+            self.request.session[settings.REMEMBER_KEY] = datetime.now().isoformat()
+            self.request.session.set_expiry(settings.REMEMBER_AGE)
+        elif is_remeber == 'off':
+            self.request.session.set_expiry(0)
+        return super(UserLoginView, self).form_valid(form)
 
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = 'register.html'
 
-def register(request):
-    if request.method == 'POST':
-        data = request.POST
-        user = User(email=data['email'], first_name=data['first_name'], last_name=data['last_name'],
-                    description=data['description'], birthday=data['birthday'], avatar=data['avatar'])
-        user.set_password(data['password'])
-        user.save()
+    def form_valid(self, form):
+        user = form.save()
         pupil = Group.objects.filter(name='Ученик')
         user.groups.set(pupil)
-        login(request, user)
-        return redirect('index.html')
-    else:
-        return render(request, 'register.html')
+        login(self.request, user)
+        return redirect('index')
+
 
 
 def log_out(request):
