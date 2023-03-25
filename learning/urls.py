9660f1cd2abd1dadf4417f594f456d7cf334b2d8
vatrushalol <vatrushkalol@gmail.com>
2023-03-25 10:50:33 +0300

    Доблавлены пути к страницам избранных уроков

--- learning/urls.py
+++ learning/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
     path('detail/<int:course_id>/', CourseDetailView.as_view(), name='detail'),
     path('enroll/<int:course_id>/', enroll, name='enroll'),
     path('update/<int:course_id>/', CourseUpdateView.as_view(), name='update'),
-    path('review/<int:course_id>/', review, name='review')
+    path('review/<int:course_id>/', review, name='review'),
+    path('<int:course_id>/create_lesson/', LessonCreateView.as_view(), name='create_lesson'),
+    path('add_booking/<int:course_id>', add_booking, name='add_booking'),
+    path('remove_booking/<int:course_id>', remove_booking, name='remove_booking'),
+    path('favourites/', FavouriteView.as_view(), name='favourites'),
+    path('settings/', SettingFormView.as_view(), name='settings'),
 ]
