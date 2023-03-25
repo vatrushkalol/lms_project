d2565e149850a9c559bff745e1dd1e97949c9249
vatrushalol <vatrushkalol@gmail.com>
2023-03-25 10:46:45 +0300

    Добавлены формы регистрации и логина

---
+++ auth_app/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import User
+from django import forms
+
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'birthday', 'description', 'avatar')
+
+
+class LoginForm(AuthenticationForm):
+
+    is_remember = forms.BooleanField(label='Запомнить', label_suffix='?', required=False)
+    class Meta:
+        model = User
+        fields = ('email', 'password')
